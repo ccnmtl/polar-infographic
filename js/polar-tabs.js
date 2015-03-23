c21e37336b8ff0ed7ce0f7712443bb94b2d46f75
James Davis <jdavis1@jamess-macbook-pro.local.ccnmtl.columbia.edu>
2015-03-23 11:06:23 -0400

    namecpacing

--- js/polar-tabs.js
+++ js/polar-tabs.js
@@ -1,59 +1,59 @@
 jQuery(document).ready(function() {  
 
-	var Column = function (tab_one, tab_two, tab_three, tab_four, title, pic, content){
-	    this.tab_1 = tab_one;
-	    this.tab_2 = tab_two;
-	    this.tab_3 = tab_three;
-	    this.tab_4 = tab_four;
-	    this.title = title;
-	    this.pic = pic;
-	    this.content = content;
-	};
 	
-    var columns = []
+    Columns = function(){
+        this.columnObject = {};
 
-    col_1 = new Column(0, 0, 0, 0, 'Column Title 1', 'path/to/pic', 'Column Body/Info Here');
-    col_2 = new Column(1, 1, 1, 1, 'Column Title 2', 'path/to/pic', 'Column Body/Info Here');
-    col_3 = new Column(2, 2, 2, 2, 'Column Title 3', 'path/to/pic', 'Column Body/Info Here');
-    col_4 = new Column(3, 3, 3, 3, 'Column Title 4', 'path/to/pic', 'Column Body/Info Here');
-    col_5 = new Column(4, 4, 4, 4, 'Column Title 5', 'path/to/pic', 'Column Body/Info Here');
-    col_6 = new Column(5, 5, 5, 5, 'Column Title 6', 'path/to/pic', 'Column Body/Info Here');
-    col_7 = new Column(6, 6, 6, 6, 'Column Title 7', 'path/to/pic', 'Column Body/Info Here');
-    col_8 = new Column(7, 7, 7, 7, 'Column Title 8', 'path/to/pic', 'Column Body/Info Here');
-    columns.push(col_1);  //columns[{}] + "<br>";
-    columns.push(col_2);
-    columns.push(col_3);
-    columns.push(col_4);
-    columns.push(col_5);
-    columns.push(col_6);
-    columns.push(col_7);
-    columns.push(col_8);
+        this.makeColumn = function(tab_one, tab_two, tab_three, tab_four, title, pic, content){
+            this.columnObject.tab_1 = tab_one;
+            this.columnObject.tab_2 = tab_two;
+            this.columnObject.tab_3 = tab_three;
+            this.columnObject.tab_4 = tab_four;
+            this.columnObject.title = title;
+            this.columnObject.pic = pic;
+            this.columnObject.content = content;
+        };
 
-    //console.log(columns);
+        this.reorderColumns = function(active_tab, active_num){
+            console.log("Inside order columns");
+            var columns = this.setColumns();
+            console.log(columns);
+            for (i = 0; i < columns.length; i++) { 
+                console.log("columns[active_num]");
+                //console.log(columns[active_num]);
+                // this will return the order the column has
+                var current_column_order = columns[active_num];
+                //console.log("current_column_order");
+                //console.log(current_column_order);
+                var column_num = '.column-' + String(active_num);
+                jQuery(active_tab).jQuery(column_num).innerHTML(columns[active_num]);
+                //columns[i]
+            }
 
-    function reorderColumns(active_tab, active_num){
-    	console.log("Inside order columns");
-        for (i = 0; i < columns.length; i++) { 
-        	console.log("columns[active_num]");
-        	console.log(columns[active_num]);
-        	// this will return the order the column has
-        	var current_column_order = columns[active_num];
-        	console.log("current_column_order");
-        	console.log(current_column_order);
-        	//var column_num = '.column-' + String(active_num);
-        	//jQuery(active_tab).jQuery(column_num).innerHTML(columns[active_num]);
-            //columns[i]
+        };
+
+        this.setColumns = function(){
+            columns = [];
+            columns[0] = this.makeColumn(0, 0, 0, 0, 'Column Title 1', 'path/to/pic', 'Column Body/Info Here');
+            columns[1] = this.makeColumn(1, 1, 1, 1, 'Column Title 2', 'path/to/pic', 'Column Body/Info Here');
+            columns[2] = this.makeColumn(2, 2, 2, 2, 'Column Title 3', 'path/to/pic', 'Column Body/Info Here');
+            columns[3] = this.makeColumn(3, 3, 3, 3, 'Column Title 4', 'path/to/pic', 'Column Body/Info Here');
+            columns[4] = this.makeColumn(4, 4, 4, 4, 'Column Title 5', 'path/to/pic', 'Column Body/Info Here');
+            columns[5] = this.makeColumn(5, 5, 5, 5, 'Column Title 6', 'path/to/pic', 'Column Body/Info Here');
+            columns[6] = this.makeColumn(6, 6, 6, 6, 'Column Title 7', 'path/to/pic', 'Column Body/Info Here');
+            columns[7] = this.makeColumn(7, 7, 7, 7, 'Column Title 8', 'path/to/pic', 'Column Body/Info Here');
+            return columns;
         }
-
     }
+
+    
 	//for (i = 0; i < 7; i++) { 
     //
     //}
     //window.setInterval(reorderColumns(), 5000);
     var counter = 1;
-    jQuery('#rotate-active-tab').on('click', function(evt){
 
-
+    jQuery('#rotate-active-tab').on('click', function(evt){ 
         var current_tab = jQuery('.nav-tabs .active');
         var current_tab_num = jQuery('.nav-tabs .active').data().tabNum;
 
@@ -67,7 +67,8 @@ jQuery(document).ready(function() {
         jQuery(next_tab_class).toggleClass("active");
         jQuery(current_tab).toggleClass("active");
         // passing currenly active class to reorder columns function
-        reorderColumns(next_tab_class, next_tab_num);
+        polarColumns = new Columns();
+        polarColumns.reorderColumns(next_tab_class, next_tab_num);
 
     });//reorderColumns());
 
