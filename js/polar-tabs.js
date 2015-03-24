c66b350e188349a4f352fc5349431373dc6e37ef
coati-00 <coati.00@gmail.com>
2015-03-24 17:37:53 -0400

    still having weird timing issues

--- js/polar-tabs.js
+++ js/polar-tabs.js
@@ -36,23 +36,22 @@ Columns = function(){
     this.columnArray = [];
     
     this.addColumn = function(tab_one, tab_two, tab_three, tab_four, new_title, pic, content){
+
     	//var json_string = String({ position: {tab_1: tab_one, tab_2: tab_two, tab_3: tab_three, tab_4: tab_four}, title: new_title, image: pic, text: content});
+    	//console.log("json_string");
+        //console.log(json_string);
     	//var json_obj = JSON.parse(json_string);
-    	var json_obj = { position: {tab_1: tab_one, tab_2: tab_two, tab_3: tab_three, tab_4: tab_four}, title: new_title, image: pic, text: content};
+    	var json_obj = { position: {tab_1: tab_one['$t'], tab_2: tab_two['$t'], tab_3: tab_three['$t'], tab_4: tab_four['$t']}, title: new_title['$t'], image: pic['$t'], text: content['$t']};
     	//console.log("json_obj");
         //console.log(json_obj);
         this.columnArray.push(json_obj);
-        console.log("this.columnArray");
-        console.log(this.columnArray);
+        //console.log("this.columnArray");
+        //console.log(this.columnArray);
     };
 }//end Columns
 
 jQuery(document).ready(function() {
     var initColumn = new Columns();
-    //initColumn.reorderColumns(0,1);
-    //initColumn.columnArray = [];
-    //console.log("initColumn.columnArray");
-    //console.log(initColumn.columnArray);
     
     url = 'https://spreadsheets.google.com/feeds/list/1rqeGy7IU7wHK5QPUzanPXr85VGwPVwa2-kmUh2is43o/od6/public/values?alt=json';
     jQuery.getJSON(url, function(data) {
@@ -67,36 +66,30 @@ jQuery(document).ready(function() {
     			               data.feed.entry[1][proj_name],
     			               data.feed.entry[0][proj_name]);
       }
-      //return initColumn;
-    });
-    console.log("initColumn");
-    console.log(initColumn);
-    console.log("initColumn.columnArray");
-    console.log(initColumn.columnArray);
-    console.log("initColumn.columnArray[0]");
-    console.log(initColumn.columnArray[0]);
-    console.log("initColumn.columnArray['0']");
-    console.log(initColumn.columnArray['0']);
+    }).always(function()
+    { //;//end of data function
 
-      window.setInterval(function() {
-        var current_tab = jQuery('.nav-tabs .active');
-        var current_tab_num = jQuery('.nav-tabs .active').data().tabNum;
-        var current_pane = ".tab-" + String(current_tab_num) + "-pane";
-        var next_tab_num = parseInt(current_tab_num) + 1;
+        window.setInterval(function() {
+            var current_tab = jQuery('.nav-tabs .active');
+            var current_tab_num = jQuery('.nav-tabs .active').data().tabNum;
+            var current_pane = ".tab-" + String(current_tab_num) + "-pane";
+            var next_tab_num = parseInt(current_tab_num) + 1;
 
-        if (next_tab_num === 5)
-        {
-            next_tab_num = 1;
-        }
+            if (next_tab_num === 5)
+            {
+                next_tab_num = 1;
+            }
         
-        var next_tab = ".tab-" + String(next_tab_num);
-        var next_pane = ".tab-" + String(next_tab_num) + "-pane";
+            var next_tab = ".tab-" + String(next_tab_num);
+            var next_pane = ".tab-" + String(next_tab_num) + "-pane";
         
-        initColumn.reorderColumns(next_tab, next_tab_num);
-        jQuery(current_tab).toggleClass("active");
-        jQuery(current_pane).toggleClass("active");
-        jQuery(next_tab).toggleClass("active");
-        jQuery(next_pane).toggleClass("active");
-      	
+            initColumn.reorderColumns(next_tab, next_tab_num);
+            jQuery(current_tab).toggleClass("active");
+            jQuery(current_pane).toggleClass("active");
+            jQuery(next_tab).toggleClass("active");
+            jQuery(next_pane).toggleClass("active");
+            console.log("initColumn.columnArray");
+            console.log(JSON.stringify(initColumn.columnArray));
       }, 2000);
+  });//end of get json always
 });
